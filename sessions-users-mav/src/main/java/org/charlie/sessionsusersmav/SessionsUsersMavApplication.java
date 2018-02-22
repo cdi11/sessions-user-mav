2a7d31f11116e46b625febd2ca935342aceb7ead
Charles Dill <31836603+cdi11@users.noreply.github.com>
2018-02-22 13:09:22 -0600

    Add files via upload

---
+++ sessions-users-mav/src/main/java/org/charlie/sessionsusersmav/SessionsUsersMavApplication.java
@@ -0,0 +1,12 @@
+package org.charlie.sessionsusersmav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionsUsersMavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionsUsersMavApplication.class, args);
+	}
+}
